982736383beb9c9b63ca215bb017e5b5555901be
Jeffery Summers <jeff@Jefferys-MBP.attlocal.net>
2024-05-10 13:18:01 -0500

    [FIX]

--- src/pages/app/components/AddNameAndDescriptionForm.tsx
+++ src/pages/app/components/AddNameAndDescriptionForm.tsx
@@ -6,7 +6,7 @@ const AddNameAndDescriptionForm = (): JSX.Element => {
     return (
         <form className={clsx('flex flex-col gap-2 self-center justify-center items-center w-full h-full')}>
             <div className={clsx('flex flex-col w-3/5  text-center')}>
-                Let's get a name for the itinerary and add a description
+                {"Let\'s get a name for the itinerary and add a description"}
                 <TextInput errorMessage="" topLeftLabel="Name:" placeholder="Monday Murder Mystery, Dave\'s Birthday, etc." />
                 <TextInput errorMessage="" topLeftLabel="Description:" placeholder="Let\'s all gather and play a murder mystery game for Dave's birthday!" />
             </div>
