df20ded21d0f1bd3b864f08f1621835f2522eafa
Jeffery Summers <jeff@Jefferys-MacBook-Pro.local>
2024-06-03 16:20:13 -0500

    [FEATURE] - create itineraries via form

--- prisma/migrations/20240531151336_create_itinerary_stored_proc/migration.sql
+++ prisma/migrations/20240531151336_create_itinerary_stored_proc/migration.sql
@@ -1,5 +1,5 @@
 CREATE FUNCTION create_itinerary(
-  p_date VARCHAR(64),
+  p_timestamp TIMESTAMP,
   p_name VARCHAR(64),
   p_description VARCHAR(256),
   p_event_names VARCHAR(64)[],
@@ -7,43 +7,53 @@ CREATE FUNCTION create_itinerary(
   p_event_ends VARCHAR(5)[],
   p_event_links VARCHAR(255)[],
   p_attendee_names VARCHAR(64)[],
-  p_attendee_emails VARCHAR(255)[],
-  p_attendee_phones VARCHAR(20)[]
+  p_attendee_emails VARCHAR(255)[]
 )
 RETURNS VOID
 AS $$
 DECLARE
-  v_itinerary_id VARCHAR(255);
   v_event_count INTEGER := array_length(p_event_names, 1);
   v_attendee_count INTEGER := array_length(p_attendee_names, 1);
   v_index INTEGER;
 BEGIN
   -- Insert the itinerary data into the database
-  INSERT INTO "Itinerary" (name, description)
-  VALUES (p_date, p_name, p_description)
-  RETURNING id INTO v_itinerary_id;
+  INSERT INTO "Itinerary" (name, date, description, "createdAt", "createdBy", "updatedAt", "updatedBy")
+  VALUES (p_name, to_char(p_timestamp, 'YYYY-MM-DD'), p_description, p_timestamp, 'system', p_timestamp, 'system');
+
   -- Insert the events data into the database
   FOR v_index IN 1..v_event_count LOOP
-    INSERT INTO "Event" (itinerary_id, name, starts, ends, link)
+    INSERT INTO "Event" (name, "itineraryName", "itineraryDate", starts, ends, link, "createdAt", "createdBy", "updatedAt", "updatedBy")
     VALUES (
-      v_itinerary_id,
       p_event_names[v_index],
+      p_name,
+      to_char(p_timestamp, 'YYYY-MM-DD'),
       p_event_starts[v_index],
       p_event_ends[v_index],
-      p_event_links[v_index]
+      p_event_links[v_index],
+      p_timestamp,
+      'system',
+      p_timestamp,
+      'system'
     );
   END LOOP;
+
   -- Insert the attendees data into the database
   FOR v_index IN 1..v_attendee_count LOOP
-    INSERT INTO "Attendee" (itinerary_id, name, email)
+    INSERT INTO "Attendee" (name, email, "itineraryName", "itineraryDate", "createdAt", "createdBy", "updatedAt", "updatedBy")
     VALUES (
-      v_itinerary_id,
       p_attendee_names[v_index],
-      p_attendee_emails[v_index]
+      p_attendee_emails[v_index],
+      p_name,
+      to_char(p_timestamp, 'YYYY-MM-DD'),
+      p_timestamp,
+      'system',
+      p_timestamp,
+      'system'
     );
   END LOOP;
-  EXCEPTION
-    WHEN OTHERS THEN
-      RAISE EXCEPTION 'Error creating itinerary: %', SQLERRM;
+
+EXCEPTION
+  WHEN OTHERS THEN
+    RAISE EXCEPTION 'Error creating itinerary: %', SQLERRM;
 END;
 $$ LANGUAGE plpgsql;
